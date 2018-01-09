9580ee48adae0d8b90132c06929b642677db7252
mile1983 <milexic@gmail.com>
2018-01-09 20:52:53 +0100

    5 komit

--- src/app/animal-list/animal-list.component.ts
+++ src/app/animal-list/animal-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Animal} from '../models/animal';
 @Component({
   selector: 'app-animal-list',
   templateUrl: './animal-list.component.html',
@@ -10,32 +10,14 @@ export class AnimalListComponent implements OnInit {
   animals:Array<Object>;
 
   constructor() { 
-    this.animals = [      
-    {
-      name:'Belka',
-      species:'svinja',
-      dob:'01.01.2001'
-    },
-    {
-      name:'Sivka',
-      species:'krava',
-      dob:'02.01.2001'
-    },
-    {
-      name:'Ridjan',
-      species:'konj',
-      dob:''
-    },
-    {
-      name:'Mekeke',
-      species:'koza',
-      dob:'04.01.2001'
-    },
-    {
-      name:'Glavonja',
-      species:'ovan',
-      dob:'05.01.2001'
-    }
+    this.animals = [  
+      new Animal('krava','Zujka','06.01.2001'),
+      new Animal('koza','Mujka','06.01.2001'),   
+      new Animal('magarac','Vujka','06.01.2001'),   
+      new Animal('morz','Kujka','06.01.2001'), 
+      new Animal('ajkula','Cujka','06.01.2001')  
+      
+   
    
     ];
   }
---
+++ src/app/models/animal.ts
@@ -0,0 +1,14 @@
+export class Animal {
+    species;
+    name;
+    dob;
+    constructor (species,name,dob)
+    {
+        this.species = species;
+        this.name = name;
+        this.dob = dob;
+        
+        
+    }
+
+}
